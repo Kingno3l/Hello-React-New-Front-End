00d729fdf6acd09424872d87906cd5e9771c04d4
kingno3l <nuelluiz@gmail.com>
2024-02-27 20:15:58 +0100

    implemented changes

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,14 +1,16 @@
 // src/Greeting.js
 
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchGreeting } from './redux/actions';
 
-const Greeting = ({ greeting, fetchGreeting }) => {
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greeting);
+
   useEffect(() => {
-    fetchGreeting();
-  }, [fetchGreeting]);
+    dispatch(fetchGreeting());
+  }, [dispatch]);
 
   return (
     <div>
@@ -17,18 +19,4 @@ const Greeting = ({ greeting, fetchGreeting }) => {
   );
 };
 
-Greeting.propTypes = {
-  greeting: PropTypes.string,
-  fetchGreeting: PropTypes.func.isRequired,
-};
-
-// Add defaultProps for the non-required prop
-Greeting.defaultProps = {
-  greeting: '', // You can set the default value as per your requirement
-};
-
-const mapStateToProps = (state) => ({
-  greeting: state.greeting,
-});
-
-export default connect(mapStateToProps, { fetchGreeting })(Greeting);
+export default Greeting;
